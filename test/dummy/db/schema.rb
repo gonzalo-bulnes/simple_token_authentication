dd75a41b24623e5f76dc2d6d5c12363d8528de57
Gonzalo Bulnes Guilpain <gon.bulnes@gmail.com>
2014-01-07 04:05:08 -0300

    Add PrivatePost, regression spec for issue #8
    I've scaffolded PrivatePost and set it as a copy of Post destined to
    be protected by token authentication.
    
    Add passing regression spec for issue #8 (PrivatePost is expected to be
    protected by token authentication and should remain protected.)

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -11,9 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140107053025) do
+ActiveRecord::Schema.define(version: 20140107064508) do
 
   create_table "posts", force: true do |t|
+    t.string   "title"
+    t.text     "body"
+    t.boolean  "published"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "private_posts", force: true do |t|
     t.string   "title"
     t.text     "body"
     t.boolean  "published"
