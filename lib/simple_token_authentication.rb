f1a011401e1dbad1ee564589d1ad0dd0c1bdf389
Gonzalo Bulnes Guilpain <gon.bulnes@gmail.com>
2014-03-16 11:27:11 -0300

    Add Rails engine behaviour

--- lib/simple_token_authentication.rb
+++ lib/simple_token_authentication.rb
@@ -1,3 +1,4 @@
+require 'simple_token_authentication/engine'
 require 'simple_token_authentication/acts_as_token_authenticatable'
 require 'simple_token_authentication/acts_as_token_authentication_handler'
 require 'simple_token_authentication/configuration'
