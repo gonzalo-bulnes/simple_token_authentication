647068e22ff721c556ef0c1f3d2ae2321c82779c
Lenart Rudel <lenart.rudel@gmail.com>
2014-05-07 21:44:07 +0200

    Refactor use .exists? when checking token existence
    Fixes #55

--- lib/simple_token_authentication/acts_as_token_authenticatable.rb
+++ lib/simple_token_authentication/acts_as_token_authenticatable.rb
@@ -18,7 +18,7 @@ module SimpleTokenAuthentication
     def generate_authentication_token
       loop do
         token = Devise.friendly_token
-        break token unless self.class.where(authentication_token: token).first
+        break token unless self.class.exists?(authentication_token: token)
       end
     end
 
