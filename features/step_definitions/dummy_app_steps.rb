38a316182829f7b64fb19e32dbc5a15b603bf70c
Gonzalo Bulnes Guilpain <gon.bulnes@gmail.com>
2014-05-24 12:55:30 -0400

    Add step: generic tok. auth. handler for controllers
    This step makes the controller configuration more visible within
    the scenario. It also allows to configure any controller as
    token autentication handler for several token authenticatable models,
    and with arbitrary sets of options.

--- features/step_definitions/dummy_app_steps.rb
+++ features/step_definitions/dummy_app_steps.rb
@@ -457,6 +457,78 @@ Given /^PrivatePostsController `acts_as_token_authentication_handler_for` (\w+)$
   }
 end
 
+Given /^PrivatePostsController `acts_as_token_authentication_handler` through:$/ do |token_authentication_configuration|
+
+  steps %Q{
+    And I overwrite "app/controllers/private_posts_controller.rb" with:
+      """
+      class PrivatePostsController < ApplicationController
+
+        # Please do notice that this controller DOES call `acts_as_authentication_handler`.
+        # See test/dummy/spec/requests/posts_specs.rb
+        #{token_authentication_configuration}
+
+        before_action :set_private_post, only: [:show, :edit, :update, :destroy]
+
+        # GET /private_posts
+        def index
+          @private_posts = PrivatePost.all
+        end
+
+        # GET /private_posts/1
+        def show
+        end
+
+        # GET /private_posts/new
+        def new
+          @private_post = PrivatePost.new
+        end
+
+        # GET /private_posts/1/edit
+        def edit
+        end
+
+        # POST /private_posts
+        def create
+          @private_post = PrivatePost.new(private_post_params)
+
+          if @private_post.save
+            redirect_to @private_post, notice: 'Private post was successfully created.'
+          else
+            render action: 'new'
+          end
+        end
+
+        # PATCH/PUT /private_posts/1
+        def update
+          if @private_post.update(private_post_params)
+            redirect_to @private_post, notice: 'Private post was successfully updated.'
+          else
+            render action: 'edit'
+          end
+        end
+
+        # DELETE /private_posts/1
+        def destroy
+          @private_post.destroy
+          redirect_to private_posts_url, notice: 'Private post was successfully destroyed.'
+        end
+
+        private
+          # Use callbacks to share common setup or constraints between actions.
+          def set_private_post
+            @private_post = PrivatePost.find(params[:id])
+          end
+
+          # Only allow a trusted parameter "white list" through.
+          def private_post_params
+            params.require(:private_post).permit(:title, :body)
+          end
+      end
+      """
+  }
+end
+
 Given /^I silence the (\w+) spec errors$/ do |controller|
   puts """
   Errors should never pass silently.
