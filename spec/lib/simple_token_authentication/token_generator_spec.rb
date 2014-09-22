d60182ad41386db8443e71d760c4cee547fc09d9
Gonzalo Bulnes Guilpain <gon.bulnes@gmail.com>
2014-09-22 13:54:36 -0300

    Add TokenGenerator public interface spec
    The `TokenGenerator` clas implements the token generator duck type.

--- spec/lib/simple_token_authentication/token_generator_spec.rb
+++ spec/lib/simple_token_authentication/token_generator_spec.rb
@@ -2,7 +2,6 @@ require 'spec_helper'
 
 describe SimpleTokenAuthentication::TokenGenerator do
 
-  it 'responds to :generate_token', protected: true do
-    expect(subject).to respond_to :generate_token
-  end
+  it_behaves_like 'a token generator'
+
 end
