6f66aaa42479cef1a65b2ed7b5fdab65602502a4
Gonzalo Bulnes Guilpain <gon.bulnes@gmail.com>
2014-09-22 13:59:06 -0300

    Add TokenGenerator private specs
    The TokenGenerator class is responsible for the token randomness and
    the fact it delegates to the Devise generator is the guaranty of its safety.
    
    If alternative token generators appear someday, that behaviour could
    become part of the class public interface.

--- spec/lib/simple_token_authentication/token_generator_spec.rb
+++ spec/lib/simple_token_authentication/token_generator_spec.rb
@@ -4,4 +4,16 @@ describe SimpleTokenAuthentication::TokenGenerator do
 
   it_behaves_like 'a token generator'
 
+  it 'delegates token generation to Devise.friendly_token', private: true do
+    devise = double()
+    allow(devise).to receive(:friendly_token).and_return('FRi3ndlY_TokeN')
+    stub_const('Devise', devise)
+
+    # delegating consists in sending the message
+    expect(Devise).to receive(:friendly_token)
+    response = subject.generate_token
+
+    # and returning the response
+    expect(response).to eq 'FRi3ndlY_TokeN'
+  end
 end
