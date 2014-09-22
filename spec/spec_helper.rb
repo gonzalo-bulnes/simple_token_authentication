3336ca21175aaf553e81b478cb9d417d9992b652
Gonzalo Bulnes Guilpain <gon.bulnes@gmail.com>
2014-09-22 13:46:58 -0300

    Enable RSpec support files

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,6 +7,8 @@ require 'active_support'
 
 require 'simple_token_authentication'
 
+Dir["./spec/support/**/*.rb"].sort.each { |f| require f; puts f }
+
 RSpec.configure do |config|
 	# some (optional) config here
 end
