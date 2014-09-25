d34e8362faabc81736a92d40950837a1d38d7553
Gonzalo Bulnes Guilpain <gon.bulnes@gmail.com>
2014-09-24 23:12:21 -0300

    Refactor specs: add dummy classes helpers to support
    Apply the move from `let` to `before` to the SimpleTokenAuthenticatable
    module specs.

--- spec/lib/simple_token_authentication/acts_as_token_authenticatable_spec.rb
+++ spec/lib/simple_token_authentication/acts_as_token_authenticatable_spec.rb
@@ -2,23 +2,16 @@ require 'spec_helper'
 
 describe 'A token authenticatable class (or one of its children)' do
 
-  let(:klass) do
-    class SomeClass; end
-    SomeClass.send :include, SimpleTokenAuthentication::ActsAsTokenAuthenticatable
+  after(:each) do
+    ensure_examples_independence
   end
 
-  let(:child_klass) do
-    class SomeChildClass < klass; end
-    SomeChildClass
-  end
-
-  let(:subjects) do
-    # all specs must apply to classes which include the module and their children
-    [klass, child_klass]
+  before(:each) do
+    define_test_subjects_for(SimpleTokenAuthentication::ActsAsTokenAuthenticatable)
   end
 
   it 'responds to :acts_as_token_authenticatable', public: true do
-    subjects.each do |subject|
+    @subjects.each do |subject|
       expect(subject).to respond_to :acts_as_token_authenticatable
     end
   end
@@ -27,7 +20,7 @@ describe 'A token authenticatable class (or one of its children)' do
 
     context 'when it acts as token authenticatable' do
       it 'ensures its instances have an authentication token before being saved', public: true do
-        subjects.each do |subject|
+        @subjects.each do |subject|
           expect(subject).to receive(:before_save).with(:ensure_authentication_token)
           subject.acts_as_token_authenticatable
         end
@@ -37,10 +30,9 @@ describe 'A token authenticatable class (or one of its children)' do
 
   describe 'instance' do
 
-    let(:subjects) { [klass.new, child_klass.new] }
-
     it 'responds to :ensure_authentication_token', protected: true do
-      subjects.each do |subject|
+      @subjects.map!{ |subject| subject.new }
+      @subjects.each do |subject|
         expect(subject).to respond_to :ensure_authentication_token
       end
     end
@@ -60,7 +52,7 @@ describe 'A token authenticatable class (or one of its children)' do
           end
         end
 
-        [klass, child_klass].each do |k|
+        @subjects.each do |k|
           k.class_eval do
 
             def initialize(args={})
@@ -87,10 +79,11 @@ describe 'A token authenticatable class (or one of its children)' do
             end
           end
         end
+        @subjects.map!{ |subject| subject.new }
       end
 
       it 'ensures its authentication token is unique', public: true do
-        subjects.each do |subject|
+        @subjects.each do |subject|
           subject.ensure_authentication_token
 
           expect(subject.authentication_token).not_to eq 'ExampleTok3n'
