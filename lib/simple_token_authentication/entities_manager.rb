88f8d606e95cc24d8c7008b356c2664aa492e417
Gonzalo Bulnes Guilpain <gon.bulnes@gmail.com>
2014-10-15 12:10:34 -0300

    Revert "Minor add EntitiesManager#entities"
    This reverts commit ab9c3f89d2b18d0b592d09e79f17fa95bf3831a6.

--- lib/simple_token_authentication/entities_manager.rb
+++ lib/simple_token_authentication/entities_manager.rb
@@ -2,19 +2,9 @@ require 'simple_token_authentication/entity'
 
 module SimpleTokenAuthentication
   class EntitiesManager
-
-    def entities
-      entities_store.values
-    end
-
     def find_or_create_entity(model)
-      entities_store[model.name] ||= Entity.new(model)
-    end
-
-    private
-
-    def entities_store
-      @entities_store ||= {}
+      @entities ||= {}
+      @entities[model.name] ||= Entity.new(model)
     end
   end
 end
