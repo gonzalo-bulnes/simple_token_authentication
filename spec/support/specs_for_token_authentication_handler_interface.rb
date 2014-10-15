2c445ac89b465b8b2c892ceef4c49899ffdac911
Gonzalo Bulnes Guilpain <gon.bulnes@gmail.com>
2014-10-15 14:56:58 -0300

    Keep private the token authentication handler interface
    It makes sense for a token auhentication handler to be some kind
    of authentication handler; one could therefore expect it to
    implement the 'authentication handler' interface.
    
    Implementing the authentication logic in a TokenAuthenticationHandler
    class (versus module), would IMO make sense, but would require a major
    renaming of public methods (starting by acts_as_token_authentication_hanlder).
    
    Such a renaming could very probably lead to a better public interface
    (e.g. setting `before_action :authenticate_user_from_token!` in
    controllers, instead of the somewhat obscure token authentication
    handling thing); however, it would _a priori_ require a major version
    jump, which I don't want to perform now.
    
    Keeping the interface private allows to decide later, and the main feature
    of this branch --the interface specs-- are as useful being private
    as being public.

--- spec/support/specs_for_token_authentication_handler_interface.rb
+++ spec/support/specs_for_token_authentication_handler_interface.rb
@@ -2,7 +2,7 @@ RSpec.shared_examples 'a token authentication handler' do
 
   let(:token_authentication_handler) { described_class }
 
-  it 'responds to :handle_token_authentication_for', public: true do
+  it 'responds to :handle_token_authentication_for', private: true do
     expect(token_authentication_handler).to respond_to :handle_token_authentication_for
   end
 end
