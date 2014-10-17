53ae40d1ad1052c8744b0a4361fd6ed086750908
Gonzalo Bulnes Guilpain <gon.bulnes@gmail.com>
2014-10-17 10:16:58 -0300

    Minor refactor authentication handlers specs
    Minor fix misspelling in the config. option interface spec

--- spec/lib/simple_token_authentication/sign_in_handler_spec.rb
+++ spec/lib/simple_token_authentication/sign_in_handler_spec.rb
@@ -4,20 +4,20 @@ describe SimpleTokenAuthentication::SignInHandler do
 
   it_behaves_like 'a sign in handler'
 
-  it 'delegates sign in to Devise::Controllers::SignInOut#sign_in through a controller', private: true do
-    controller = double()
-    allow(controller).to receive(:sign_in).with(:record, option: 'some_value').and_return('Devise response.')
-    allow(controller).to receive(:env).and_return({})
-
-    # delegating consists in sending the message
-    expect(controller).to receive(:sign_in)
-    response = subject.sign_in(controller, :record, option: 'some_value')
-
-    # and returning the response
-    expect(response).to eq 'Devise response.'
-  end
-
   describe '#sign_in' do
+
+    it 'delegates sign in to Devise::Controllers::SignInOut#sign_in through a controller', private: true do
+      controller = double()
+      allow(controller).to receive(:sign_in).with(:record, option: 'some_value').and_return('Devise response.')
+      allow(controller).to receive(:env).and_return({})
+
+      # delegating consists in sending the message
+      expect(controller).to receive(:sign_in)
+      response = subject.sign_in(controller, :record, option: 'some_value')
+
+      # and returning the response
+      expect(response).to eq 'Devise response.'
+    end
 
     it 'integrates with Devise trackable', protected: true do
       controller = double()
