a14b645b569cef59771e11757ed465bb418f6a3b
Gonzalo Bulnes Guilpain <gon.bulnes@gmail.com>
2016-04-20 19:44:48 +1000

    Update CHANGELOG
    Minor fix misspelling

--- lib/simple_token_authentication/exception_fallback_handler.rb
+++ lib/simple_token_authentication/exception_fallback_handler.rb
@@ -2,7 +2,7 @@ module SimpleTokenAuthentication
   class ExceptionFallbackHandler
     include Singleton
 
-    # Notifies the failure of authentication to Warden in the same DEvise does.
+    # Notifies the failure of authentication to Warden in the same Devise does.
     # Does result in an HTTP 401 response in a Devise context.
     def fallback!(controller, entity)
       throw(:warden, scope: entity.name_underscore.to_sym) if controller.send("current_#{entity.name_underscore}").nil?
