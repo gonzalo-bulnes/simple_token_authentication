e8f517726f98bcfe5b1b62e98fcc76d3fa7557c1
Gonzalo Bulnes Guilpain <gon.bulnes@gmail.com>
2016-07-15 17:27:50 +1000

    Add :after_successful_token_authentication hook

--- spec/support/specs_for_token_authentication_handler_interface.rb
+++ spec/support/specs_for_token_authentication_handler_interface.rb
@@ -5,4 +5,11 @@ RSpec.shared_examples 'a token authentication handler' do
   it 'responds to :handle_token_authentication_for', private: true do
     expect(token_authentication_handler).to respond_to :handle_token_authentication_for
   end
+
+  describe 'instance' do
+
+    it 'responds to :after_successful_token_authentication', hooks: true, private: true do
+      expect(token_authentication_handler.new).to respond_to :after_successful_token_authentication
+    end
+  end
 end
