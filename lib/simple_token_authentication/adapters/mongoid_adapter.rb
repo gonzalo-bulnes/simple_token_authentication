0c1a9a596731f4c8358a4874d9d6c54cfbc5017f
Gonzalo Bulnes Guilpain <gon.bulnes@fastmail.com>
2019-08-20 18:51:28 +1000

    Prevent Mongoid adapter from loading when unsupported
    Unless I'm wrong, no version of Mongoid is currently compatible with Rails 6.
    
    As far as I know at the time of writing, the XML serializer requirement
    is currently unmet, and I think it is a reasonable flag to use in order
    to load the adpater only when usable.
    
    Note: the syntax of the condition must be compatible with Ruby 1.9.3

--- lib/simple_token_authentication/adapters/mongoid_adapter.rb
+++ lib/simple_token_authentication/adapters/mongoid_adapter.rb
@@ -1,13 +1,20 @@
-require 'mongoid'
-require 'simple_token_authentication/adapter'
+if Module.const_defined?('ActiveModel') &&
+  ActiveModel.const_defined?('Serializers') &&
+  ActiveModel::Serializers.const_defined?('Xml')
+  # As far as I know Mongoid doesn't support Rails 6
+  # Please let me know if this isn't true when you read it!
 
-module SimpleTokenAuthentication
-  module Adapters
-    class MongoidAdapter
-      extend SimpleTokenAuthentication::Adapter
+  require 'mongoid'
+  require 'simple_token_authentication/adapter'
 
-      def self.base_class
-        ::Mongoid::Document
+  module SimpleTokenAuthentication
+    module Adapters
+      class MongoidAdapter
+        extend SimpleTokenAuthentication::Adapter
+
+        def self.base_class
+          ::Mongoid::Document
+        end
       end
     end
   end
--- spec/lib/simple_token_authentication/adapters/mongoid_adapter_spec.rb
+++ spec/lib/simple_token_authentication/adapters/mongoid_adapter_spec.rb
@@ -1,21 +1,28 @@
-require 'spec_helper'
-require 'simple_token_authentication/adapters/mongoid_adapter'
+if Module.const_defined?('ActiveModel') &&
+  ActiveModel.const_defined?('Serializers') &&
+  ActiveModel::Serializers.const_defined?('Xml')
+  # As far as I know Mongoid doesn't support Rails 6
+  # Please let me know if this isn't true when you read it!
 
-describe 'SimpleTokenAuthentication::Adapters::MongoidAdapter' do
+  require 'spec_helper'
+  require 'simple_token_authentication/adapters/mongoid_adapter'
 
-  before(:each) do
-    stub_const('Mongoid', Module.new)
-    stub_const('Mongoid::Document', double())
+  describe 'SimpleTokenAuthentication::Adapters::MongoidAdapter' do
 
-    @subject = SimpleTokenAuthentication::Adapters::MongoidAdapter
-  end
+    before(:each) do
+      stub_const('Mongoid', Module.new)
+      stub_const('Mongoid::Document', double())
 
-  it_behaves_like 'an adapter'
+      @subject = SimpleTokenAuthentication::Adapters::MongoidAdapter
+    end
 
-  describe '.base_class' do
+    it_behaves_like 'an adapter'
 
-    it 'is Mongoid::Document', private: true do
-      expect(@subject.base_class).to eq Mongoid::Document
+    describe '.base_class' do
+
+      it 'is Mongoid::Document', private: true do
+        expect(@subject.base_class).to eq Mongoid::Document
+      end
     end
   end
 end
